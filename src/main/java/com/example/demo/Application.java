571db0a020d26755fede94b165754fc7261af54e
gabbythomas <gabrient@gmail.com>
2021-07-11 04:53:11 +0000

    fix default project to have an endpoint to hit when deployed

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,30 @@
+
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@RestController
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+  public static void main(String[] args) {
+      SpringApplication.run(Application.class, args);
+  }
+  
+  @RequestMapping("/hello")
+  public String hello(){
+      return "Hello world!";
+  }
+
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(Application.class);
+  }
+}
